4f9be9e248d2abcda65fe055dd3eb7ccf726015f
Alan Yao <alanyao.training@gmail.com>
2021-07-01 00:04:54 -0700

    Add nav bar

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Header.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            Tactics Ninja
+          </Navbar.Brand>
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="#solve">Solve Puzzles</Nav.Link>
+              <Nav.Link href="#changelog">Changelog</Nav.Link>
+              <NavDropdown title="Help" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/question">Ask a Question</NavDropdown.Item>
+                <NavDropdown.Item href="#action/suggestion">Make a Suggestion</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/bug">Report a Bug</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+          
+          <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
