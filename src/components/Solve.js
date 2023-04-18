50ee383dae95ad2b582ee21c946491e38ab47c4f
Alan Yao <70783523+alaney2@users.noreply.github.com>
2023-04-18 01:40:07 -0500

    Update Solve.js

--- src/components/Solve.js
+++ src/components/Solve.js
@@ -2,7 +2,7 @@ function Solve() {
   return <div className="grid grid-cols-1 md:grid-cols-4">
     <div className="text-xl text-center p-8 md:col-start-2 col-span-2">
       <h1>
-        Coming later... a tool where you can upload a photo of a real chess board for analysis!
+        Coming later (maybe not) ... a tool where you can upload a photo of a real chess board for analysis!
       </h1>
     </div>
     
