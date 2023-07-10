adcf2b3115da368f7b8cfe7aab2e8cb260e878fe
Alan Yao <alanyao.training@gmail.com>
2023-07-09 17:50:28 -0700

    analytics

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Analytics>
+      <App />
+    </Analytics>
   </React.StrictMode>,
   document.getElementById('root')
 );
